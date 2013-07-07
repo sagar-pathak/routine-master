ac62ccc2e0e62c5a68d7988da2c03b84106e5c07
Sagar Pathak <surfer.sagar@gmail.com>
2013-07-07 23:20:32 +0545

    group added

--- operation/create_routine.php
+++ operation/create_routine.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
   Document   : create_routine
   Created on : Jun 21, 2013, 11:19:21 PM
@@ -13,59 +12,64 @@ $redirected_to = '/routine-master/home.php?opt=1';
 $department_name = $_COOKIE['department_name'];
 $semester = $_COOKIE['semester'];
 $table_name = 'courses';
-$query = 'SELECT DISTINCT department_id FROM department WHERE department_name = "'.$department_name.'"';
+$table_name1 = 'routine';
+$query = 'SELECT DISTINCT department_id FROM department WHERE department_name = "' . $department_name . '"';
 $result_set_first = mysql_query($query);
 $row = mysql_fetch_array($result_set_first);
 $department_id = $row['department_id'];
-$query = 'SELECT course_code, course_title FROM '.$table_name.' WHERE (department_id = '.$department_id.'  AND semester = '.$semester.' )';
+$query = 'SELECT course_code, course_title FROM ' . $table_name . ' WHERE (department_id = ' . $department_id . '  AND semester = ' . $semester . ' )';
 $result = mysql_query($query);
-$query1 = 'SELECT course_code, course_title FROM '.$table_name.' WHERE (department_id = '.$department_id.'  AND semester = '.$semester.' AND lab = 1 )';
+$query1 = 'SELECT course_code, course_title FROM ' . $table_name . ' WHERE (department_id = ' . $department_id . '  AND semester = ' . $semester . ' AND lab = 1 )';
 $result1 = mysql_query($query1);
+$query2 = 'SELECT DISTINCT group FROM ' . $table_name1;
+$result2 = mysql_query($query2);
 
 //INSERTING ROUTINE INTO TABLE
-if(isset($_POST['set_routine'])){
+if (isset($_POST['set_routine'])) {
     $day = $_POST['day'];
     $count = $_POST['count'];
     $i = 1;
-    while($i <= $count){
-        if((isset($_POST['choose_this_course_'.$i])) && ($_POST['choose_this_course_'.$i] == 'yes')){
-            $course_code = $_POST['course_code_'.$i];
-            $time = $_POST['time_to_n_from_'.$i];
-            
+    while ($i <= $count) {
+        if ((isset($_POST['choose_this_course_' . $i])) && ($_POST['choose_this_course_' . $i] == 'yes')) {
+            $course_code = $_POST['course_code_' . $i];
+            $time = $_POST['time_to_n_from_' . $i];
+
             $query = 'INSERT INTO `routine`(`course_code`, `department_id`, 
-                `semester`, `day`, `from_and_to`) VALUES ("'.$course_code.'",'.$department_id.',
-                '.$semester.',"'.$day.'","'.$time.'")';
-            if(mysql_query($query)){
+                `semester`, `day`, `from_and_to`) VALUES ("' . $course_code . '",' . $department_id . ',
+                ' . $semester . ',"' . $day . '","' . $time . '")';
+            if (mysql_query($query)) {
                 
-            }else{
+            } else {
                 die(mysql_error());
                 exit(0);
             }
         }
         $i++;
     }
-    setcookie('success_notifier',$day,  time()+3600,'/');
-    header('Location: '.$redirected_to);
+    setcookie('success_notifier', $day, time() + 3600, '/');
+    header('Location: ' . $redirected_to);
+} else if (isset($_POST['new_group_set'])) {
+    setcookie('group_name', $_POST['new_group'], time() + 3600, '/');
+    header('Location: ' . $redirected_to);
 }
 ?>
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-    
-<div class="container">
-    <?php
-    if(isset($_COOKIE['success_notifier'])){
-        echo '<div class="table-row">
-             <div class="col information-box" style="width:70%">Routine has been successfully created for '.$_COOKIE['success_notifier'].'</div> 
-        </div><br/>';
-        setcookie('success_notifier','',  time()-3600,'/');
-    }
-    ?>
-</div>
+
     <div class="container">
-    <div class="table-row">
-        <div class="col" style="width:10%"></div>
-         <div class="col" style="width:20%">Choose Day:</div>
-         <div class="col" style="width:50%"><p>
-             <select name="day">
+        <?php
+        if (isset($_COOKIE['success_notifier'])) {
+            echo '<div class="table-row">
+             <div class="col information-box" style="width:70%">Routine has been successfully created for ' . $_COOKIE['success_notifier'] . '</div> 
+        </div><br/>';
+            setcookie('success_notifier', '', time() - 3600, '/');
+        }
+        ?>
+    </div>
+    <div class="container">
+        <div class="table-row">
+            <div class="col" style="width:23%">Choose Day:</div>
+            <div class="col">
+                <select name="day">
                     <option value="Sunday">Sunday</option>
                     <option value="Monday">Monday</option>
                     <option value="Tuesday">Tuesday</option>
@@ -74,43 +78,75 @@ if(isset($_POST['set_routine'])){
                     <option value="Friday">Friday</option>
                     <option value="Saturday">Saturday</option>
                 </select>
-             </p>
-         </div>
-         <div class="col" style="width:20%"></div> 
+
+            </div>
+            <div class="col" style="width:20%"></div> 
+        </div>
     </div>
-    <div class="heading">
-         <div class="col" style="width:10%">Select</div>
-         <div class="col" style="width:20%">Course Code</div>
-         <div class="col" style="width:50%">Course Title</div>
-         <div class="col" style="width:20%">Time</div>
+    <div class="container">
+        <div class="table-row">
+            <div class="col" style="width:23%">Choose Group:</div>
+            <div class="col">
+                <select name="group_name">
+                    <?php
+                    while ($row = mysql_fetch_array($result2)) {
+                       
+                            echo '<option>' . $row['group'] . '</option>';
+                        
+                    }
+                    if (isset($_COOKIE['group_name'])) {
+                        $new_group = $_COOKIE['group_name'];
+                        echo '<option>' . $new_group . '</option>';
+                    }
+                    ?>
+                </select>
+
+            </div>
+            <div class="col" style="width:20%"></div> 
+        </div>
+        <div class="table-row">
+            <div class="col">Specify new group(if any):</div>
+            <div class="col"><input type="text" name="new_group"/>
+                <input style="width:23%;" type="submit" value="SET" name="new_group_set"/></div>
+            <div class="col"></div>
+        </div>
     </div>
-    <?php
-    $count = 1;
-    while($row = mysql_fetch_array($result)){
-        echo '<div class="table-row">
-            <div class="col" style="width:10%"><input type="checkbox" name="choose_this_course_'.$count.'" value="yes" /></div>
-             <div class="col" style="width:20%"><input type="hidden" value="'.$row['course_code'].'" name="course_code_'.$count.'"/> '.$row['course_code'].'</div>
-             <div class="col" style="width:50%">'.$row['course_title'].'</div>
-             <div class="col" style="width:20%"><input type="text" name="time_to_n_from_'.$count.'" /></div>
+    <p>
+    <div class="container">
+        <div class="heading">
+            <div class="col" style="width:10%">Select</div>
+            <div class="col" style="width:20%">Course Code</div>
+            <div class="col" style="width:50%">Course Title</div>
+            <div class="col" style="width:20%">Time</div>
+        </div>
+        <?php
+        $count = 1;
+        while ($row = mysql_fetch_array($result)) {
+            echo '<div class="table-row">
+            <div class="col" style="width:10%"><input type="checkbox" name="choose_this_course_' . $count . '" value="yes" /></div>
+             <div class="col" style="width:20%"><input type="hidden" value="' . $row['course_code'] . '" name="course_code_' . $count . '"/> ' . $row['course_code'] . '</div>
+             <div class="col" style="width:50%">' . $row['course_title'] . '</div>
+             <div class="col" style="width:20%"><input type="text" name="time_to_n_from_' . $count . '" /></div>
         </div>';
-        $count ++;
-    }
-    while($row = mysql_fetch_array($result1)){
-        echo '<div class="table-row">
-             <div class="col" style="width:10%"><input type="checkbox" name="choose_this_course_'.$count.'" value="yes" /></div>
-             <div class="col" style="width:20%"><input type="hidden" value="'.$row['course_code'].'[LAB]" name="course_code_'.$count.'"/> '.$row['course_code'].' [LAB]</div>
-             <div class="col" style="width:50%">'.$row['course_title'].' [LAB]</div>
-             <div class="col" style="width:20%"><input type="text" name="time_to_n_from_'.$count.'" /></div>
+            $count++;
+        }
+        while ($row = mysql_fetch_array($result1)) {
+            echo '<div class="table-row">
+             <div class="col" style="width:10%"><input type="checkbox" name="choose_this_course_' . $count . '" value="yes" /></div>
+             <div class="col" style="width:20%"><input type="hidden" value="' . $row['course_code'] . '[LAB]" name="course_code_' . $count . '"/> ' . $row['course_code'] . ' [LAB]</div>
+             <div class="col" style="width:50%">' . $row['course_title'] . ' [LAB]</div>
+             <div class="col" style="width:20%"><input type="text" name="time_to_n_from_' . $count . '" /></div>
         </div>';
-        $count ++;
-    }
-    ?>
-     <div class="table-row">
-         <div class="col" style="width:10%"><input type="hidden" name="count" value="<?php echo $count;?>"/></div>
-         <div class="col" style="width:20%"><input type="submit" name="set_routine" value="SET ROUTINE" /></div>
-         <div class="col" style="width:50%"></div>
-         <div class="col" style="width:20%"></div>
-      
+            $count++;
+        }
+        ?>
+        <div class="table-row">
+            <div class="col" style="width:10%"><input type="hidden" name="count" value="<?php echo $count; ?>"/></div>
+            <div class="col" style="width:20%"><input type="submit" name="set_routine" value="SET ROUTINE" /></div>
+            <div class="col" style="width:50%"></div>
+            <div class="col" style="width:20%"></div>
+
+        </div>
     </div>
-</div>
+</p>
 </form>
