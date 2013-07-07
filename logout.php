28157ad2bcb9a6120d6356e1d4f6b272cff6ab01
Sagar Pathak <surfer.sagar@gmail.com>
2013-07-07 19:38:17 +0545

    deletion of cookie added

--- logout.php
+++ logout.php
@@ -17,8 +17,9 @@ if (isset($_COOKIE['new_branch'])) {
 unset($_SESSION['logged_in']);
 unset($_SESSION['user_id']);
 unset($_SESSION['error']);
-setcookie("department_name", " ", time() - 3600);
-setcookie("semester", " ", time() - 3600);
+setcookie("department_name", " ", time() - 3600,"/");
+setcookie("semester", " ", time() - 3600,"/");
+setcookie("c_no", " ", time() - 3600,"/");
 session_destroy();
 header('Location: login.php');
 ?>
